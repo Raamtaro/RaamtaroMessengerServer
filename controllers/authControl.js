e6976d2cb4608e81debb821e08af033705a4c1b5
Raam <raam.sanghani@gmail.com>
2024-09-04 17:12:32 -0500

    Auth signup route finished

--- controllers/authControl.js
+++ controllers/authControl.js
@@ -1,4 +1,4 @@
-import expressAsyncHandler from "express-async-handler";
+import asyncHandler from "express-async-handler";
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 import passport from "passport";
@@ -11,7 +11,52 @@ configDotenv();
 
 const prisma = new PrismaClient()
 
+const userSignup = asyncHandler( async (req, res) => {
+
+    const {email, password, name} = req.body
+    //if they haven't submitted the fields, then throw an error
+    if (!email || !password || !name) {
+        return res.status(400).json({feedback: "Please include email, password and name"})
+    }
+
+    //Is there an existing user?
+    const existingUser = await prisma.user.findUnique(
+        {
+            where: {
+                email: email
+            }
+        }
+    )
+    if (existingUser) {
+        return res.status(400).json({feedback: `account with email: ${email} already exists.`})
+    }
+
+    
+    //If it passes the above checks, then prepare the data.
+    const hashedPassword = await bcrypt.hash(password, 11)
+    const userData = {
+        name, email, password: hashedPassword
+    }
+
+
+    //Create user with new Data
+    const newUser = await prisma.user.create(
+        {
+            data: userData
+        }
+    )
+
+    res.status(201).json(
+        {
+            newUser
+        }
+    )
+
+
+
+})
+
 
 export default {
-    
+    userSignup
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import passport from "passport";
+import authControl from "../controllers/authControl.js";
 
 const router = Router()
+
+
+router.post('/signup', authControl.userSignup)
 
 export default router
