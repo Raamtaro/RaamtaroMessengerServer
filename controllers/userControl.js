4ef3f63e16367436c27606720716ae882b858601
Raam <raam.sanghani@gmail.com>
2024-09-10 14:56:28 -0500

    User routes for get all, get one, and delete are set up and hooked up, need to test

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -74,7 +74,44 @@ const updateUserPassword = asyncHandler( async(req, res) => { //Moving this to t
 })
 
 const deleteUser = asyncHandler( async(req, res) => {
+
+    const client = req.user
+    const id = parseInt(req.params.id)
+
+    if (!id) {
+        return res.status(400).json(
+            {
+                error: "Invalid Request."
+            }
+        )
+    }
+
+    if (client.id !== id) {
+        return res.status(403).json(
+            {
+                error: "Cannot delete someone else's profile."
+            }
+        )
+    }
     
+    try {
+        await prisma.user.delete(
+            {
+                where: {
+                    id: id
+                }
+            }
+        )
+        res.status(204).json({message: "User successfully deleted"})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(
+            {
+                error: "Couldn't delete user"
+            }
+        )
+    }
+
 })
 
 export default {
