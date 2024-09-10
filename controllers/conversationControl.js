38a767931cc52fd30e4d4b71f4b1201c734bdcff
Raam <raam.sanghani@gmail.com>
2024-09-10 15:10:06 -0500

    Set up template for conversations. The conversations controller will need to account for a few different types of functionalities which may be better added as entirely separate routes for the endpoint.

--- controllers/conversationControl.js
+++ controllers/conversationControl.js
@@ -4,3 +4,31 @@ import { PrismaClient } from "@prisma/client";
 import { validationResult } from "express-validator";
 
 const prisma = new PrismaClient()
+
+const getConversations = asyncHandler( async (req, res) => {
+
+})
+
+const getConversation = asyncHandler( async (req, res) => {
+    
+})
+
+const createConversation = asyncHandler( async (req, res) => {
+    
+})
+
+const updateConversation = asyncHandler( async (req, res) => { 
+    
+})
+
+const deleteConversation = asyncHandler( async (req, res) => {
+    
+})
+
+export default {
+    getConversations,
+    getConversation,
+    createConversation,
+    updateConversation,
+    deleteConversation
+}
--- routes/conversations.js
+++ routes/conversations.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import passport from "passport";
+import conversationControl from "../controllers/conversationControl.js";
+
 
 const router = Router()
 
-// router.get()
-// router.post()
-// router.put()
-// router.delete()
+router.get('/conversations', passport.authenticate('jwt', {session: false}), conversationControl.getConversations)
+router.get('/:id', passport.authenticate('jwt', {session: false}), conversationControl.getConversation)
+router.post('/create', passport.authenticate('jwt', {session: false}), conversationControl.createConversation)
+router.put('/:id', passport.authenticate('jwt', {session: false}), conversationControl.updateConversation)
+router.delete('/:id', passport.authenticate('jwt', {session: false}), conversationControl.deleteConversation)
 
 export default router
