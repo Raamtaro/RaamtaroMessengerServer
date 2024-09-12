494692574257923087dad6217ab698088f969c48
Raam <raam.sanghani@gmail.com>
2024-09-12 14:37:09 -0500

    Basic conversation create and read. Need to now add/consider functionality for adding participant (probably just one at a time).

--- controllers/conversationControl.js
+++ controllers/conversationControl.js
@@ -7,6 +7,24 @@ const prisma = new PrismaClient()
 
 const getConversations = asyncHandler( async (req, res) => {
 
+    /**
+     * Likely just something that will be useful. Since I'm returning every conersation in the db, I'll omit the messages since this will be every conversation. maybe just conversations and titles
+     */
+    const allConversations = await prisma.conversation.findMany()
+    res.status(200).json({
+        allConversations
+    })
+})
+
+const getUserConversations = asyncHandler( async (req, res) => {
+    /**
+     * This should retrieve the conversations which a user has authored (authorId), AND the ones which they've participated in
+     * Therefore, we'll use an OR rule - in this case, we'll findMany({where: {OR: [participants contains the user's id, authorId = client.id]}})
+     * This will return ALL conversations associated with the user
+     */
+
+
+
 })
 
 const getConversation = asyncHandler( async (req, res) => {
@@ -14,7 +32,23 @@ const getConversation = asyncHandler( async (req, res) => {
 })
 
 const createConversation = asyncHandler( async (req, res) => {
-    
+    const client = req.user
+    const {title} = req.body
+    const conversationData = {}
+
+    if (title) conversationData.title = title
+
+    const newConversation = await prisma.conversation.create(
+        {
+            data: {
+                ...conversationData,
+                author: {connect: {id: client.id}}
+            },
+        }
+    )
+    res.status(201).json({newConversation})
+
+
 })
 
 const updateConversation = asyncHandler( async (req, res) => { 
@@ -27,6 +61,7 @@ const deleteConversation = asyncHandler( async (req, res) => {
 
 export default {
     getConversations,
+    getUserConversations,
     getConversation,
     createConversation,
     updateConversation,
