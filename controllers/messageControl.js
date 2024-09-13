3d8b2db6cadb183c6fb9cafd82c539b7b8bfed56
Raam <raam.sanghani@gmail.com>
2024-09-13 13:05:42 -0500

    No major updates, added the basic tools to the remaining handler files (message/profile)Control.js

---
+++ controllers/messageControl.js
@@ -0,0 +1,6 @@
+import asyncHandler from "express-async-handler";
+import { PrismaClient } from "@prisma/client";
+
+import { validationResult } from "express-validator";
+
+const prisma = new PrismaClient()
