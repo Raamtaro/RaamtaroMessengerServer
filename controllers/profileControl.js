456cd6c03354db17e69c61b4196a1cdc014cd122
Raam <raam.sanghani@gmail.com>
2024-09-17 14:28:47 -0500

    Finished the profile endpoint, need to test functionality. I am also unsure of the delete and update methods for the profile endpoint, as it feels like there should be some extra security checks. For the PUT route in particular, I also need to consider whether the user is uploading a file as part of their request. I might even end up making a separate route for this entirely as it kind of has its own logic/workflow.

--- controllers/profileControl.js
+++ controllers/profileControl.js
@@ -31,7 +31,19 @@ const getMyProfile = asyncHandler( async (req, res) => {
 })
 
 const getProfile = asyncHandler( async (req, res) => {
-    
+    const id = parseInt(req.params.id)
+    const profile = await prisma.profile.findUnique(
+        {
+            where: {
+                id: id
+            }
+        }
+    )
+    res.status(200).json(
+        {
+            profile
+        }
+    )
 })
 
 const createProfile = asyncHandler( async (req, res) => {
@@ -75,11 +87,67 @@ const createProfile = asyncHandler( async (req, res) => {
 })
 
 const updateProfile = asyncHandler( async (req, res) => {
-    
+    /**
+     * Realistically, only the user should be updating their own profile
+     */
+
+    const client = req.user
+    const {bio} = req.body //I'm going to have to consider cloudinary logic here for uploading files and then including the cloud URL into the data as the pointee in the sql db
+
+    const updateData = {}
+    if(bio) {
+        updateData.bio = bio
+    }
+
+    const updatedProfile = await prisma.profile.update(
+        {
+            where: {
+                ownerId: client.id
+            },
+
+            data: {
+                ...updateData
+            }
+        }
+    )
+    res.status(200).json(
+        {
+            updatedProfile
+        }
+    )
+
 })
 
 const deleteProfile = asyncHandler( async (req, res) => {
-    
+    /**
+     * This would probably only be necessary when we are deleting an account.
+     */
+
+    const client = req.user
+    const id = parseInt(req.params.userId)
+
+    if (id !== client.id) {
+        return res.status(403).json(
+            {
+                error: "Forbidden"
+            }
+        )
+    }
+
+    await prisma.profile.delete(
+        {
+            where:
+            {
+                ownerId: id
+            }
+        }
+    )
+
+    res.status(200).json(
+        {
+            message: "Profile deleted."
+        }
+    )
 })
 
 export default {
--- routes/profile.js
+++ routes/profile.js
@@ -9,7 +9,7 @@ router.get('/profiles', passport.authenticate('jwt', {session: false}), profileC
 router.get('/mine', passport.authenticate('jwt', {session: false}), profileControl.getMyProfile)
 router.get('/:id', passport.authenticate('jwt', {session: false}), profileControl.getProfile)
 router.post('/create', passport.authenticate('jwt', {session: false}), profileControl.createProfile)
-router.put('/:id', passport.authenticate('jwt', {session: false}), profileControl.updateProfile)
-router.delete('/:id', passport.authenticate('jwt', {session: false}), profileControl.deleteProfile)
+router.put('/', passport.authenticate('jwt', {session: false}), profileControl.updateProfile) //not including the id param here as the update should only be done by the owner themselves, thus we should just grab the id from req.user
+router.delete('/:userId', passport.authenticate('jwt', {session: false}), profileControl.deleteProfile)
 
 export default router
