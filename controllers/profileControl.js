858251fc4ccfed620789b1a27f5b285962743b65
Raam <raam.sanghani@gmail.com>
2024-09-17 14:08:35 -0500

    Need to re-deploy the Schema in railway - updated the profile model to add an @unqiue identifier to the ownerId field. This helps to re-enforce data integrity. I'm surprised that I haven't had to do this with anything else yet, but I might go through and add @unique to any of the other models where it makes sense - such as authorIds for both messages and conversations

--- controllers/profileControl.js
+++ controllers/profileControl.js
@@ -6,11 +6,28 @@ import { validationResult } from "express-validator";
 const prisma = new PrismaClient()
 
 const getProfiles = asyncHandler( async (req, res) => {
-
+    const allProfiles = await prisma.profile.findMany()
+    res.status(200).json(
+        {
+            allProfiles
+        }
+    )
 })
 
 const getMyProfile = asyncHandler( async (req, res) => {
-    
+    const client = req.user
+    const myProfile = await prisma.profile.findUnique(
+        {
+            where: {
+                ownerId: client.id
+            }
+        }
+    )
+    res.status(200).json(
+        {
+            myProfile
+        }
+    )
 })
 
 const getProfile = asyncHandler( async (req, res) => {
@@ -18,7 +35,43 @@ const getProfile = asyncHandler( async (req, res) => {
 })
 
 const createProfile = asyncHandler( async (req, res) => {
-    
+    const client = req.user
+    //Get the user and check if they already have a profile
+
+    const user = await prisma.user.findUnique(
+        {
+            where: {
+                id: client.id
+            },
+            include: {
+                profile: true
+            }
+        }
+    )
+
+    if (user.profile.length) {
+        return res.status(403).json(
+            {
+                error: "Forbidden"
+            }
+        )
+    }
+
+    const profile = await prisma.profile.create(
+        {
+            data: {
+                owner: {connect: {id: client.id}}
+            }
+        }
+    )
+
+    res.status(201).json(
+        {
+            profile
+        }
+    )
+
+
 })
 
 const updateProfile = asyncHandler( async (req, res) => {
--- routes/profile.js
+++ routes/profile.js
@@ -6,9 +6,9 @@ import profileControl from "../controllers/profileControl.js";
 const router = Router()
 
 router.get('/profiles', passport.authenticate('jwt', {session: false}), profileControl.getProfiles)
-router.get('/myProfile', passport.authenticate('jwt', {session: false}), profileControl.getMyProfile)
+router.get('/mine', passport.authenticate('jwt', {session: false}), profileControl.getMyProfile)
 router.get('/:id', passport.authenticate('jwt', {session: false}), profileControl.getProfile)
-router.post('/create/:userId', passport.authenticate('jwt', {session: false}), profileControl.createProfile)
+router.post('/create', passport.authenticate('jwt', {session: false}), profileControl.createProfile)
 router.put('/:id', passport.authenticate('jwt', {session: false}), profileControl.updateProfile)
 router.delete('/:id', passport.authenticate('jwt', {session: false}), profileControl.deleteProfile)
 
